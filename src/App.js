6ba00758f40358f6e6031244ff17d4e9fc2074d1
Piotr Kowalski <piokowam@gmail.com>
2021-06-12 16:40:00 +0200

    change css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Tasks from "./Tasks";
 import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
-import Conteiner from "./Conteiner";
+import Container from "./Container";
 
 const tasks = [
   { id: 1, content: "przejść na Reacta", done: false },
@@ -14,7 +14,7 @@ const hideDoneTasks = false;
 
 function App() {
   return (
-    <Conteiner>
+    <Container>
       <Header title="Lista zadań" />
       <Section
         title="Dodaj nowe zadanie"
@@ -30,7 +30,7 @@ function App() {
           <Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />
         }
       />
-    </Conteiner>
+    </Container>
   );
 }
 
